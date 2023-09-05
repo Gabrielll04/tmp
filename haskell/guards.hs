a737b34971172245fff821446d089352fed5550a
Gabriel Guimarães Carneiro <107888562+Gabrielll04@users.noreply.github.com>
2023-09-05 16:58:19 -0300

    Create guards.hs

---
+++ haskell/guards.hs
@@ -0,0 +1,29 @@
+-- http://learnyouahaskell.com/syntax-in-functions#guards-guards
+
+-- Guards are a way of testing whether an value are true or false.
+-- It's very familiar with if statements but more readable.
+-- Guards play really nicely with patterns.
+
+bmi :: (RealFloat a) => a -> a -> String
+bmi w h -- Yes, there's no '=' right after the function name and its parameters
+    | w / h^ 2 <= 18.25 = "You're underweight, you emo, you!"
+    | w / h^ 2 <= 25.0  = "You're supposedly normal. Pffff, I bet you're ugly!"
+    | w / h^ 2 <= 30    = "You're fat!"
+    | otherwise         = "You're a whale, congrats!"
+    
+max' :: (Ord a) => a -> a -> a
+max' a b 
+    | a > b     = a
+    | otherwise = b
+    
+myCompare :: (Ord a) => a -> a -> Ordering
+a `myCompare` b
+    | a > b     = GT 
+    | a == b    = EQ
+    | otherwise = LT
+    
+main :: IO ()
+main = do
+    print (bmi 54 1.70)
+    print (max' 5 4)
+    print (5 `myCompare` 3)
