d412bf3a8f2e77e08942c2baf270e23c5132a081
Gabriel Guimarães Carneiro <107888562+Gabrielll04@users.noreply.github.com>
2023-09-17 21:51:04 -0300

    Update fibonacci.hs

--- haskell/fibonacci.hs
+++ haskell/fibonacci.hs
@@ -1,6 +1,13 @@
 fibs :: [Int]
 fibs = [ a + b | (a,b) <- zip (1: fibs) (0: 1: fibs) ] -- Creating a list of tuple and returning the sum of the numbers from each tuples.
 
+-- My own version
+fibonacci :: Int -> Int -> Int -> [Int]
+fibonacci a b n
+  | n <= 0 = []
+  | otherwise = b:fibonacci b (a+b) (n-1)
+
+
 main :: IO ()
 main = do
   print (take 10 fibs)
