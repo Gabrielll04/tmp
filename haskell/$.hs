43035f378eec32dc655904f72506b7ffbc10cad2
Gabriel Guimarães Carneiro <107888562+Gabrielll04@users.noreply.github.com>
2023-09-24 15:11:53 -0300

    Removing useless space on $.hs

--- haskell/$.hs
+++ haskell/$.hs
@@ -3,7 +3,6 @@
 -- Consider the expression sum (map sqrt [1..130]). Because $ has such a low precedence, we can rewrite that expression as sum $ map sqrt [1..130]
 -- sqrt 3 + 4 + 9? This adds together 9, 4 and the square root of 3. If we want get the square root of 3 + 4 + 9, we'd have to write sqrt (3 + 4 + 9) or if we use $ we can write it as sqrt $ 3 + 4 + 9 because $ has the lowest precedence of any operator. 
 
-
 main :: IO ()
 main =  do
     print (map ($ 3) [(4-), (10*), (^2), sqrt])
