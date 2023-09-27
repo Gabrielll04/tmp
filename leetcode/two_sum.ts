788632e5cda57529e531a589ea805b1484cf4a8b
gabrius <107888562+Gabrielll04@users.noreply.github.com>
2023-09-27 18:24:31 -0300

    Renaming 'ii' variable to 'j'

--- leetcode/two_sum.ts
+++ leetcode/two_sum.ts
@@ -1,12 +1,10 @@
 function twoSum(nums: number[], target: number): any {
     for (let i = 0; i < nums.length; i++) {
         let a = nums[i]
-
-        for (let ii = i+1; ii < nums.length; ii++) {
-            let b = nums[ii]
-
+        for (let j = i+1; j < nums.length; j++) {
+            let b = nums[j]
             if (a + b == target) {
-              return [i, ii]
+              return [i, j]
             }
         }
     }
