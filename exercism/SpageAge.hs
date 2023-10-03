0d1746d18d471c85edefb0b68afac1b974b295ba
gabrius <107888562+Gabrielll04@users.noreply.github.com>
2023-10-03 17:51:52 -0300

    Update SpageAge.hs

--- exercism/SpageAge.hs
+++ exercism/SpageAge.hs
@@ -9,8 +9,10 @@ data Planet = Mercury
 
 ageOn :: Planet -> Float -> Float
 ageOn Earth seconds = (seconds / 86400) / 365.25
-ageOn Mercury seconds = (seconds / 86400*2) / 87.969257175 -- That's not finished.
-
-main :: IO ()
-main =  do
-    print(ageOn Mercury 2134835688)
+ageOn Mercury seconds = (seconds / 86400) / 87.969257175
+ageOn Venus seconds = (seconds / 86400) / 224.700799215
+ageOn Mars seconds = (seconds / 86400) / 686.96797095
+ageOn Jupiter seconds = (seconds / 86400) / 4332.82012875
+ageOn Saturn seconds = (seconds / 86400) / 10755.6986445
+ageOn Uranus seconds = (seconds / 86400) / 30687.1530015
+ageOn Neptune seconds = (seconds / 86400) / 60190.02963
