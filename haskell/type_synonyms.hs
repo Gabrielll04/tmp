676527530584c01332796f9c299441240dd50ed6
gabrius <107888562+Gabrielll04@users.noreply.github.com>
2023-10-05 21:41:47 -0300

    Create type_synonyms.hs

---
+++ haskell/type_synonyms.hs
@@ -0,0 +1,32 @@
+import qualified Data.Map as Map (Map)
+
+-- With types synonyms we can give a different name to a type. Exemple: type String = [Char]. Note that we're not actually making anything new, but just a synonym.
+
+-- Setting a type for your list PhoneBook that we've created on Data.Map charpter:
+
+type Phone = String-- Giving the String type synonyms is something that Haskell programmers often do  when they want to convey more informations about what is the exacly kind of string that is expected
+type Name = String
+type PhoneBook = [(Name,Phone)]
+
+phoneBook :: PhoneBook
+phoneBook =
+    [("betty","555-2938")     
+    ,("bonnie","452-2928")     
+    ,("patsy","493-2928")     
+    ,("lucille","205-2928")     
+    ,("wendy","939-8282")     
+    ,("penny","853-2492")     
+    ] 
+    
+inPhoneNumber :: Name -> Phone -> PhoneBook ->  Bool
+inPhoneNumber name pnumber pbook = (name,pnumber) `elem` pbook
+    
+-- Type synonyms can also be parameterized. If we want a type that represents an association list type but still want it to be general so it can use any type as the keys and values, we can do this:
+type AssocList k v = [(k,v)] -- (Eq k) => k -> AssocList k v -> Maybe v
+
+-- If we wanted a type that represents a map (from Data.Map) from integers to something, we could either do this:
+type IntMap v = Map.Map Int v
+    
+main :: IO ()
+main = do
+    print (inPhoneNumber "betty" "555-2938" phoneBook) 
