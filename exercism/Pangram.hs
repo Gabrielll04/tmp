00abeb931329186857bc67004971825d63475c5a
gabrius <107888562+Gabrielll04@users.noreply.github.com>
2023-10-11 21:20:57 -0300

    Update Pangram.hs

--- exercism/Pangram.hs
+++ exercism/Pangram.hs
@@ -1,22 +1,21 @@
-import Data.List (isInfixOf, splitAt, concat)
-import Data.Char (isSpace, isMark, isNumber)
-
+module Pangram (isPangram) where
+import Data.List
+import Data.Char
+rmDup :: String -> String-- I know that's horrible solution, I'm just a beginner man :(
 rmDup [] = []
-rmDup (x:xs) = x : rmDup (filter (\x -> not(isNumber x)) (filter (\x -> x /= '_') (filter (\x -> not(isMark x)) (filter (\x -> not(isSpace x)) (filter (\y -> not(x == y)) xs))))) -- I know that's horrible, I'm just a  beginner man :(
-
+rmDup arr@(x:xs) 
+    | not (validChar x) = rmDup xs
+    | (x `elem` xs) == True = rmDup xs
+    | otherwise         = x : rmDup (filter validChar xs) 
+    where validChar c = not (isNumber c) && c /= '_' && not (isSpace c) && not (isPunctuation c) && (isAscii c)
+    
 quicksort :: (Ord a) => [a] -> [a]
 quicksort [] = []
 quicksort (x:xs) = 
   let lowerSorted = quicksort [a | a <- xs, a <= x]
       upperSorted = quicksort [a | a <- xs, a > x]
   in lowerSorted ++ [x] ++ upperSorted
-
 isPangram :: String -> Bool
 isPangram text
-    | ['a'..'z'] == quicksort (rmDup text) = True
-    | otherwise                            = False
-    
-main :: IO ()
-main = do   
-    print (isPangram "abcdefghijklmnopqrstuvwxyz")
-    print (rmDup "abcdefghijklmnopqrstuvwxyz")
+    | ['a'..'z'] == quicksort (rmDup $ map toLower text) = True
+    | otherwise                                          = False
