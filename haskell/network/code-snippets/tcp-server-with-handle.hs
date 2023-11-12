2f513157bbc5c6364c8adc09ba8f4b4bf1f921d1
gabrius <107888562+Gabrielll04@users.noreply.github.com>
2023-11-12 16:14:29 -0300

    Create tcp-server-with-handle.hs

---
+++ haskell/network/code-snippets/tcp-server-with-handle.hs
@@ -0,0 +1,16 @@
+import Network.Socket
+import Network.Socket.ByteString
+
+main :: IO ()
+main = do
+  let hints = defaultHints { addrFlags = [AI_NUMERICHOST, AI_NUMERICSERV], addrSocketType = Stream } -- defaultHints returns a addrInfo
+  addr:_ <- getAddrInfo (Just hints) (Just "127.0.0.1") (Just "1234")
+  sock <- socket (addrFamily addr) (addrSocketType addr) (addrProtocol addr)
+  bind sock (addrAddress addr) -- Bounding
+  listen sock 2
+  (conn, _) <- accept sock
+  hdl <- socketToHandle conn ReadWriteMode
+  hSetBuffering hdl NoBuffering
+  hPutStrLn hdl "Welcome to the server"
+  hClose hdl
+  close sock
