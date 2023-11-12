0192b4538582d6f514094f2b8d9148bdc71d3e5c
gabrius <107888562+Gabrielll04@users.noreply.github.com>
2023-11-12 16:17:56 -0300

    Create tcp-server-with-send.hs

---
+++ haskell/network/code-snippets/tcp-server-with-send.hs
@@ -0,0 +1,16 @@
+import Network.Socket
+import Network.Socket.ByteString
+import Data.ByteString.Char8 -- to use pack
+
+main :: IO () -- "SockAddr to print the "getSocketName sock
+main = do
+  let hints = defaultHints { addrFlags = [AI_NUMERICHOST, AI_NUMERICSERV], addrSocketType = Stream } -- defaultHints returns a addrInfo
+  addr:_ <- getAddrInfo (Just hints) (Just "127.0.0.1") (Just "1234")
+  sock <- socket (addrFamily addr) (addrSocketType addr) (addrProtocol addr)
+  bind sock (addrAddress addr) -- Bounding
+  listen sock 5 -- Listening
+  (conn, _) <- accept sock -- Accepting a connection. The socket must be bound to an address and listening for connections.
+  let msg = pack "HTTP/1.1 200 OK\n\nWelcome"
+  send conn msg
+  close conn
+  close sock
