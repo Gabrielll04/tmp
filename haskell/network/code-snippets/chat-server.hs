15846c546c9e975f21b4b3ebbb277ed51af8bf6e
gabrius <107888562+Gabrielll04@users.noreply.github.com>
2023-11-17 21:04:30 -0300

    Create chat-server.hs

---
+++ haskell/network/code-snippets/chat-server.hs
@@ -0,0 +1,56 @@
+import System.IO 
+import Network.Socket
+import Control.Exception
+import Control.Monad.Fix 
+import Control.Concurrent
+import Control.Monad (when)
+import Control.Concurrent.Chan
+
+main :: IO ()
+main = do
+  let hints = defaultHints { addrFlags = [AI_PASSIVE], addrSocketType = Stream }
+  addr:_ <- getAddrInfo (Just hints) (Just "127.0.0.1") (Just "8080")
+  sock <- socket (addrFamily addr) (addrSocketType addr) (addrProtocol addr)
+  bind sock (addrAddress addr)
+  listen sock 5
+  chan <- newChan
+  _ <- forkIO $ fix $ \loop -> do
+    (_, _) <- readChan chan
+    loop
+  mainLoop sock chan 0
+
+type Msg = (Int, String)
+
+mainLoop :: Socket -> Chan Msg -> Int -> IO ()
+mainLoop sock chan msgNum = do
+  (conn, _) <- accept sock
+  forkIO $ runConn conn chan msgNum
+  mainLoop sock chan $! msgNum + 1
+
+runConn :: Socket -> Chan Msg -> Int -> IO ()
+runConn conn chan msgNum = do
+  let broadcast msg = writeChan chan (msgNum, msg)
+  hdl <- socketToHandle conn ReadWriteMode
+  hSetBuffering hdl NoBuffering
+  
+  hPutStrLn hdl "What's yous name? "
+  name <- fmap init (hGetLine hdl)
+  hPutStrLn hdl ("Welcome " ++ name ++ "!")
+
+  commLine <- dupChan chan
+
+  reader <- forkIO $ fix $ \loop -> do
+    (nextNum, line) <- readChan commLine
+    when (msgNum /= nextNum) $ hPutStrLn hdl line
+    loop
+
+  handle (\(SomeException _) -> return()) $ fix $ \loop -> do
+    line <- fmap init (hGetLine hdl)
+    case line of
+      -- If an exception is caught, send a message and break the loop
+      "quit" -> hPutStrLn hdl "Bye!"
+      _      -> broadcast (name ++ ": " ++ line) >> loop
+
+  killThread reader
+  broadcast ("--> " ++ name ++ " left")
+  hClose hdl
