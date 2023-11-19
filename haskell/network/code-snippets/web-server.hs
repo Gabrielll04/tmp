691aa6d5177fbd4e774eaecb5d282394d2dcb6a9
gabriel <107888562+Gabrielll04@users.noreply.github.com>
2023-11-19 02:03:34 -0300

    Update web-server.hs to send strings in ByteString instead handles

--- haskell/network/code-snippets/web-server.hs
+++ haskell/network/code-snippets/web-server.hs
@@ -1,18 +1,26 @@
-import Network.Socket
-import System.IO
-import Control.Monad (forever)
-import Control.Concurrent (forkIO)
+module Main where
 
-main :: IO ()
+import Network.Socket
+import Control.Concurrent(forkIO)
+import Data.ByteString.Char8 (pack)
+import Network.Socket.ByteString (send)
+
+
 main = do
-    let hints = defaultHints { addrFlags = [AI_PASSIVE], addrSocketType = Stream }
-    addr:_ <- getAddrInfo (Just hints) (Just "127.0.0.1") (Just "8080")
-    sock <- socket (addrFamily addr) (addrSocketType addr) (addrProtocol addr)
-    bind sock (addrAddress addr)
-    listen sock 5
-    forever $ do
-        (conn,_) <- accept sock
-        hdl <- socketToHandle conn ReadWriteMode
-        hSetBuffering hdl NoBuffering
-        hPutStrLn hdl "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<h1>Welcome to the Jungle! :D</h1><style>h1 {color: red;}</style><p>How are you?</p>"
-        hClose hdl 
+  let hints = defaultHints { addrFlags = [AI_PASSIVE], addrSocketType = Stream }
+  addr:_ <- getAddrInfo (Just hints) (Just "0.0.0.0") (Just "8080")
+  sock <- socket (addrFamily addr) (addrSocketType addr) (addrProtocol addr)
+  bind sock (addrAddress addr)
+  listen sock 1024
+  mainLoop sock
+
+mainLoop :: Socket -> IO ()
+mainLoop sock = do
+  (conn, _) <- accept sock
+  forkIO $ runConn conn
+  mainLoop sock
+
+runConn :: Socket -> IO ()
+runConn conn = do -- Sending the string with send to improve performance
+  send conn (pack "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<h1>Welcome to the Jungle! :D</h1><style>h1 {color: red;}</style><p>How are you?</p>")
+  close conn
