d15962184a99f609b3f53b5450bb2e232ab4d103
gabriel <107888562+Gabrielll04@users.noreply.github.com>
2025-04-28 20:17:51 -0300

    Update convert_temp.rs

--- rust/convert_temp.rs
+++ rust/convert_temp.rs
@@ -1,40 +1,39 @@
 use std::io;
 
-fn getNumbers(tempToConvert: char) -> Result<String, f64> {
+fn read_numbers(temp_to_convert: char) -> Result<f64, String> {
     let mut n1: String = String::new();
     
-    println!("Insira a temperatura em {tempToConvert}");
+    println!("Insira a temperatura em {temp_to_convert}");
     
     io::stdin()
         .read_line(&mut n1)
-        .expect("Ocorreu um erro ao ler a temperatura inserita");
+        .expect("Ocorreu um erro ao ler a temperatura inserida");
 
     // work in progress, writing this bc im bored
-    // match n1.parse()::f64 {
-    //     Ok(num) => Ok(num),
-    //     Err(_)
-    // } 
-    
-    // let n1: Result<String, f64> = match n1.trim().parse() {
-    //     Ok(num) => num,
-    //     Err(_) => Err("Ocorreu um erro ao converter o valor".toString()),
-    // };
-    
-    return n1;
+    match n1.trim().parse::<f64>() {
+        Ok(num) => {
+            match temp_to_convert {
+                'F' => Ok((num - 32.) * 5.0 / 9.0),
+                'C' => Ok(num * 9.0 / 5.0 + 32.0),
+                _ => Err("Opção inválida!".to_string()),
+            }
+        },
+        Err(_) => Err("Ocorreu um erro ao converter o valor recebido!".to_string()),
+    } 
 }
 
-fn f2c() {
-    return getNumbers('F');
+fn f2c() -> Result<f64, String> {
+    return read_numbers('F');
 }
 
-fn c2f() {
-    return getNumbers('C');
+fn c2f() -> Result<f64, String> {
+    return read_numbers('C');
 }
 
 fn menu() {
-    let mut op: String = String::new();
-    
     loop {
+        let mut op: String = String::new();
+    
         println!("MENU");
         println!("1 - Converter de F para C");
         println!("2 - Converter de C para F");
@@ -45,12 +44,21 @@ fn menu() {
     
         let op: u8 = match op.trim().parse() {
             Ok(num) => num,
-            Err(_) => continue,
+            Err(_) => {
+                println!("Entrada inválida. Tente novamente.");
+                continue;
+            },
         };
     
         match op {
-            1 => f2c(),
-            2 => c2f(),
+            1 => match f2c() {
+                Ok(result) => println!("Resultado: {:.2} °C", result),
+                Err(e) => println!("{}", e),
+            },
+            2 => match c2f() {
+                Ok(result) => println!("Resultado: {:.2} °F", result),
+                Err(e) => println!("{}", e)
+            }
             _ => println!("Você deve inserir uma opção existente!"),
         };
     }
